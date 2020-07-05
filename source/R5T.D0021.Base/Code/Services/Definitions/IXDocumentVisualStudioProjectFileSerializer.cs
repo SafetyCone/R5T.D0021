551e3520de9515a969223aa3078bac6e32d72acd
MinexAutomation <minex.automation@gmail.com>
2020-07-05 00:06:30 -0700

    Added R5T.D0021.Base library. Added R5T.D0013.Base (IFileSerializer<T>) and R5T.T0004 (XDocumentVisualStudioProjectFile) project-reference dependencies.

---
+++ source/R5T.D0021.Base/Code/Services/Definitions/IXDocumentVisualStudioProjectFileSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.D0013;
+using R5T.T0004;
+
+
+namespace R5T.D0021
+{
+    public interface IXDocumentVisualStudioProjectFileSerializer : IFileSerializer<XDocumentVisualStudioProjectFile>
+    {
+    }
+}
